0aa5f685c56be53851ddef70d3fd6491f41b86e6
Santha Aadhira <74529097+aadhira636@users.noreply.github.com>
2026-04-09 16:45:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  const nameVar = "John";
+  const ageVar = 25;
+
+  return (
+    <>
+      <Child name={nameVar} age={ageVar} />
+    </>
+  );
+}
+export default App;
+
+function Child(props) {
+  console.log(props.age);
+  return (
+    <div>
+      <Grandchild name={props.name} age={props.age} />
+    </div>
+  );
+}
+
+function Grandchild(props) {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Name: {props.name}</h1>
+      <h1>Age: {props.age}</h1>
+    </div>
+  );
+}
+
